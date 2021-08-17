03d865d8e4d31d88ef474ac0082a828523264d92
lauri.lehtola <mr.lauri.lehtola@gmail.com>
2021-08-17 10:37:32 +0300

    Initial commit

---
+++ src/main/java/com/experis/experiscodingtestback/ExperisCodingTestBackApplication.java
@@ -0,0 +1,13 @@
+package com.experis.experiscodingtestback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperisCodingTestBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExperisCodingTestBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/experis/experiscodingtestback/ExperisCodingTestBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experis.experiscodingtestback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperisCodingTestBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
