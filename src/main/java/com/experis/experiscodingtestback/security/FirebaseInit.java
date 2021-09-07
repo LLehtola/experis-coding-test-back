c6a0d7e68214c17c2c4bf01f0c66d919fe843ac1
Satuhoo <heikkonen.satu@gmail.com>
2021-09-07 15:27:37 +0300

    cut: remove firebase config and add new one to env. variables

--- src/main/java/com/experis/experiscodingtestback/security/FirebaseInit.java
+++ src/main/java/com/experis/experiscodingtestback/security/FirebaseInit.java
@@ -3,21 +3,26 @@ package com.experis.experiscodingtestback.security;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 @Service
 public class FirebaseInit {
 
+    @Value("${GOOGLE_CREDENTIALS}")
+    private String gservicesConfig;
+
+
     @PostConstruct
     private void initialize() {
         try {
-            FileInputStream serviceAccountJSON =
-                    new FileInputStream("src/main/resources/firebase_config.json");
+            InputStream is = new ByteArrayInputStream(gservicesConfig.toString().getBytes());
             FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccountJSON))
+                    .setCredentials(GoogleCredentials.fromStream(is))
                     .build();
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
